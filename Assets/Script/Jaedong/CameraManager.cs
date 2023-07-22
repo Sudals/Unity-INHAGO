f689aad339abfdc0ab8a6f61f0c032f0ff40fc67
jdong800@naver.com <jdong800@naver.com>
2023-07-22 16:15:00 +0900

    캐릭터 이동 방법, 플랫폼 변경

--- Assets/Script/Jaedong/CameraManager.cs
+++ Assets/Script/Jaedong/CameraManager.cs
@@ -5,36 +5,17 @@ using UnityEngine;
 
 public class CameraManager : MonoBehaviour
 {
-    private Vector3 touchStartPosition;
-    public float moveSpeed = 100000000000000.0f;
-
     public Transform target;
     public Vector3 offset;
 
+    public int camSpeed = 10;
+
     void Update()
     {
-        if (Input.touchCount > 0)
-        {
-            Touch touch = Input.GetTouch(0);
+        float camRotate = Input.GetAxis("Mouse Y");
+        transform.Rotate(Vector3.up * camSpeed * Time.deltaTime * camRotate);
 
-            if (touch.phase == TouchPhase.Began)
-            {
-                touchStartPosition = touch.position;
-            }
-            else if (touch.phase == TouchPhase.Moved)
-            {
-                Vector3 touchCurrentPosition = touch.position;
-                Vector3 touchDelta = touchCurrentPosition - touchStartPosition;
 
-                float moveX = touchDelta.x * Time.deltaTime * moveSpeed;
-                float moveZ = touchDelta.y * Time.deltaTime * moveSpeed;                
-
-                transform.Translate(new Vector3(moveX, 0.0f, moveZ));
-
-                touchStartPosition = touchCurrentPosition;
-            }
-        } else {
-            transform.position = target.position + offset;
-        }
+        transform.position = target.position + offset;
     }
 }
--- Assets/Script/Jaedong/PlayerManager.cs
+++ Assets/Script/Jaedong/PlayerManager.cs
@@ -3,73 +3,48 @@ using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.EventSystems;
 
-public class PlayerManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+public class PlayerManager : MonoBehaviour
 {
-    public GameObject player;
+    public float speed = 2.0f, jumpPower = 13.0f;
 
-    public string buttonName;
-    public float speed = 10.0f, jumpPower = 0.1f;      
+    private Vector3 moveVec;
+    private float hAxis, vAxis;
 
     private new Rigidbody rigidbody;
     private Animator animator;
 
-    private bool    isMovingForward = false, isMovingLeft = false,
-                    isMovingBack = false, isMovingRight = false, isJump = false;
+    private bool isRunDown, isJumpDown, isJump = false;
 
     // Start is called before the first frame update
     void Awake()
-    {
-        rigidbody = player.GetComponent<Rigidbody>();
-        animator = player.GetComponent<Animator>();      
+    {        
+        rigidbody = GetComponent<Rigidbody>();
+        animator = GetComponent<Animator>();      
     }
 
     // Update is called once per frame
     void Update()
     {
-        if (isMovingForward) MoveForward();
-        else if (isMovingLeft) MoveLeft();
-        else if (isMovingBack) MoveBack();
-        else if (isMovingRight) MoveRight();       
-    }
+        hAxis = Input.GetAxisRaw("Horizontal");
+        vAxis = Input.GetAxisRaw("Vertical");
+        isRunDown = Input.GetButton("Run");
+        isJumpDown = Input.GetButtonDown("Jump");
+        
+        moveVec = new Vector3(hAxis, 0, vAxis).normalized;
 
-    private void MoveForward()
-    {
-        float v = 1;
-        Vector3 vector = new Vector3(0, 0, v).normalized;
+        if (isRunDown) transform.position += moveVec * speed * Time.deltaTime * 2;
+        else transform.position += moveVec * speed * Time.deltaTime;
 
-        player.transform.position += speed * Time.deltaTime * vector;
-        player.transform.LookAt(player.transform.position + vector);
-    }
+        transform.LookAt(transform.position + moveVec);
 
-    private void MoveLeft()
-    {
-        float h = -1;
-        Vector3 vector = new Vector3(h, 0, 0).normalized;
+        animator.SetBool("isWalk", moveVec != Vector3.zero);
+        animator.SetBool("isRun", isRunDown || isJump);
 
-        player.transform.position += speed * Time.deltaTime * vector;
-        player.transform.LookAt(player.transform.position + vector);
-    }
-
-    private void MoveBack()
-    {
-        float v = -1;
-        Vector3 vector = new Vector3(0, 0, v).normalized;
-
-        player.transform.position += speed * Time.deltaTime * vector;
-        player.transform.LookAt(player.transform.position + vector);
-    }
-
-    private void MoveRight()
-    {
-        float h = 1;
-        Vector3 vector = new Vector3(h, 0, 0).normalized;
-
-        player.transform.position += speed * Time.deltaTime * vector;
-        player.transform.LookAt(player.transform.position + vector);
+        if (isJumpDown && !isJump) Jump();
     }
 
     private void Jump()
-    {        
+    {       
         isJump = true;
         rigidbody.AddForce(Vector3.up * jumpPower, ForceMode.Impulse);        
     }
@@ -77,41 +52,5 @@ public class PlayerManager : MonoBehaviour, IPointerDownHandler, IPointerUpHandl
     private void OnCollisionEnter(Collision collision)
     {        
         if (collision.collider.gameObject.CompareTag("Floor")) isJump = false;                
-    }
-
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if(buttonName != "Jump Button") animator.SetBool("isRun", true);
-
-        switch (buttonName)
-        {
-            case "W Button":
-                isMovingForward = true;
-                break;
-            case "A Button":
-                isMovingLeft = true;
-                break;
-            case "S Button":
-                isMovingBack = true;
-                break;
-            case "D Button":
-                isMovingRight = true;
-                break;
-            case "Jump Button":                
-                if (!isJump) Jump();
-                break;
-            default:
-                break;
-        }
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        animator.SetBool("isRun", false);
-
-        isMovingForward = false;
-        isMovingLeft = false;
-        isMovingBack = false;
-        isMovingRight = false;       
     }
 }
